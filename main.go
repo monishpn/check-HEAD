22728e308495034f38e485c56d49e92889c8f868
monishpn <monish.pn@zop.dev>
2025-06-11 17:54:43 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Charlie"
+	age := 25
+	height := 1.75
+	isStudent := true
+
+	fmt.Printf("Name: %s, Age: %d\n", name, age)
+	fmt.Printf("Height: %.2f meters\n", height) // .2f formats to 2 decimal places
+	fmt.Printf("Is student: %t\n", isStudent)
+	fmt.Printf("Name's type: %T\n", name)
+	fmt.Printf("Value and Type: %v, %T\n", age, age)
+}
